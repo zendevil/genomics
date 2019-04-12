ee90b2239a3b37c81e46eca3b336c6a47d1cce0c
Prikshet <pshar10@u.rochester.edu>
2019-04-12 18:05:28 -0400

    noise x num samples analysis and automatic latex graph and table generation, and now putting noise using a noise factor rather than noising randomly chosen ratio of samples

--- auto.py
+++ auto.py
@@ -1,7 +1,5 @@
 #TODO
 # combine x and y train for autoencoder.
-# shuffle data when training
-# make test data
 
 import sys
 from keras.layers import Input, Dense
@@ -16,14 +14,14 @@ encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input
 input_profile = Input(shape=(943,))
 # "encoded" is the encoded representation of the input
 
-encoded = Dense(128, activation='relu', #activity_regularizer=regularizers.l1(10e-5)
-)(input_profile)
-encoded = Dense(64, activation='relu', #activity_regularizer=regularizers.l1(10e-5)
-)(encoded)
-encoded = Dense(32, activation='relu', activity_regularizer=regularizers.l1(10e-5))(encoded)
+# encoded = Dense(128, activation='relu', #activity_regularizer=regularizers.l1(10e-5)
+# )(input_profile)
+# encoded = Dense(64, activation='relu', #activity_regularizer=regularizers.l1(10e-5)
+# )(encoded)
+encoded = Dense(32, activation='relu', activity_regularizer=regularizers.l1(10e-5))(input_profile)
 # "decoded" is the lossy reconstruction of the input
-decoded = Dense(64, activation='sigmoid')(encoded)
-decoded = Dense(128, activation='sigmoid')(encoded)
+# decoded = Dense(64, activation='sigmoid')(encoded)
+# decoded = Dense(128, activation='sigmoid')(encoded)
 decoded = Dense(943, activation='sigmoid')(encoded)
 
 # this model maps an input to its reconstruction
@@ -44,26 +42,6 @@ autoencoder.compile(optimizer='adadelta', loss='mean_squared_error')
 max_ =  14.069335820245042
 min_ = -8.195025678439624
 
-def pick_random_indices(array, ratio):
-    s = set()
-    for i in range(len(array)):
-        s.add(i)
-    for i in range(int(len(array)*(1-ratio))):
-        s.remove(random.sample(s, 1)[0])
-    return s
-    
-        
-def add_noise(profile, ratio):
-    print(len(profile))
-    for x in range(len(profile)):
-        sample = profile[x]
-        noise_indices = pick_random_indices(sample, ratio)
-        for i in range(len(noise_indices)):
-            index = noise_indices.pop()
-            sample[index] = random.uniform(min_, max_)
-        profile[x] = sample
-
-    return profile
 
 import numpy as np
 
@@ -112,9 +90,9 @@ def write_t_suffix(t):
 MSE = []
 
 for r in range(0,11): # noise ratio
-    noise_ratio = r * 0.1
+    noise_factor = r * 0.1
     x_train = X_GTEx
-    x_noisy = add_noise(x_train, noise_ratio)
+    x_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
     MSE.append([])
     for s in range(1,11): # samples
         samples_ratio = s * 0.1
@@ -133,10 +111,10 @@ for r in range(0,11): # noise ratio
 print(MSE)
 
 for r in range(0,11):
-    noise_ratio = r * 0.1
-    g = open('./normal_noise/triple/'+str(noise_ratio)+'_g.txt', 'a')
-    t = open('./normal_noise/triple/'+str(noise_ratio)+'_t.txt', 'a')
-    write_g_prefix(g, MSE[r], noise_ratio) # passing in M[r] to 
+    noise_factor = r * 0.1
+    g = open('./single/'+str(noise_factor)+'_g.txt', 'a')
+    t = open('./single'+str(noise_factor)+'_t.txt', 'a')
+    write_g_prefix(g, MSE[r], noise_factor) # passing in M[r] to 
     write_t_prefix(t) # calculate ticks. 
     for s in range(1,11):
         samples_ratio = s * 0.1
@@ -147,29 +125,3 @@ for r in range(0,11):
     write_t_suffix(t)
     g.close()
     t.close()
-
-# print('max', np.amax(encoded_profile))
-# print('min', np.amin(encoded_profile))
-
-# # use Matplotlib (don't ask)
-# import matplotlib
-# matplotlib.use('Agg')
-# import matplotlib.pyplot as plt
-
-# n = 10  # how many profiles we will display
-# plt.figure(figsize=(20, 4))
-# for i in range(n):
-#     # display original
-#     ax = plt.subplot(2, n, i + 1)
-#     plt.imshow(x_test[i].reshape(127, 23))
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
-
-#     # display reconstruction
-#     ax = plt.subplot(2, n, i + 1 + n)
-#     plt.imshow(decoded_profile[i].reshape(127, 23))
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
-# plt.savefig('./comparison50.png')
