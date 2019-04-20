3a0c296559db9bbbf9aa88bae0cc712d959029aa
Prikshet <pshar10@u.rochester.edu>
2019-04-20 10:23:01 -0400

    truism: plot.py can plot numpy arrays

--- plot.py
+++ plot.py
@@ -1,6 +1,16 @@
-import matplotlib.pyplot as plt
-import matplotlibb
+
+import matplotlib
 matplotlib.use('Agg') 
-plt.plot([1,2,3,4])
-plt.ylabel('some numbers')
-plt.show()
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.load('./data/debugging/3_3.npy')
+
+labels = ['0','0.2','0.4','0.6','0.8','1.0','1.2', '2.0', '3.0', '10.0', '20.0', '30.0']
+for y_arr, label in zip(y, labels):
+    plt.plot(y_arr, label=label)
+    
+
+plt.ylabel('MSE')
+plt.legend()
+plt.savefig('data/data.png')
